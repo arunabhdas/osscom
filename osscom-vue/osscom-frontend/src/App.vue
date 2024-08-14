ca874e9fd1ef76efadbd1686a600eca1ceb4c91a
Arunabh Das <arunabhdas@gmail.com>
2024-08-14 12:12:06 -0400

    Added Contact screen

--- osscom-vue/osscom-frontend/src/App.vue
+++ osscom-vue/osscom-frontend/src/App.vue
@@ -14,6 +14,7 @@ import HomeDefault from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
       </nav>
     </div>
   </header>
---
+++ osscom-vue/osscom-frontend/src/views/ContactView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+const header = ref('Contact')
+
+const items = ref([
+  {id: 1, label: "10 simple list items"},
+  {id: 2, label: "5 tile list items"},
+  {id: 3, label: "3 scroll list items"}
+])
+
+const default_items = ref({
+  'item-1': {id: 1, label: "RecyclerView"},
+  'item-2': {id: 2, label: "ScrollView"},
+  'item-3': {id: 3, label: "ListView"}
+})
+
+const newItem = ref("")
+
+const newItemPriority = ref("low")
+
+const newItemDoneState = ref(false)
+
+const newItemHighPriority = ref(false)
+
+</script>
+<template>
+  <div class="contact">
+    <h1>{{ header }}</h1>
+    <input v-model.trim="newItem" type="text" placeholder="Add an item">
+    newItemPriority
+    {{newItem}}
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority">
+      High Priority
+    </label>
+    <br/>
+    {{newItemHighPriority}}
+    <ul>
+      <li v-for="({id, label}, index) in items" :key="id">
+        {{label}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .contact {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
